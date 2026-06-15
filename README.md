180a6a2fc7bd085fb4178501f5f304ecbb022afa
nadeemaibuddy <nadeem.aibuddy@gmail.com>
2026-06-15 10:20:42 +0000

    ch-3

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python
+https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/YouTube/The%20Ultimate%20Python%20Handbook.pdf
---
+++ string.py
@@ -0,0 +1,51 @@
+#string is data type
+#imutable
+
+a ='nadeem'
+b="nadeem"
+c="""nadeem"""
+
+name=a[0:4]   #stating :ending:increment(back word start from  -1)
+print(name)
+print(len(a))
+print(a[-5:-1])
+print(a[1:]) #same as a[1:6]
+print(a[:4]) #same as a[0:4]
+
+print(a[1:6:2])
+
+
+print("hi\n i am nadeem") #\n new line
+#\t tab
+# \"\" 
+
+
+#function
+print(len(a))
+print(a.endswith("eem"))
+print(b.startswith("na"))
+print(c.capitalize()) #only captilize only first letter
+print(a.lower())
+print(b.upper())
+print(a.count("e"))
+print(a.find("a"))
+print(a.replace("nadeem","NADEEM"))
+
+
+
+name1=input("enter your name")
+print(f"good afternoon {name1}")
+
+letter = '''
+Dear <|Name|>,
+You are selected!
+<|Date|>'''
+print(letter.replace("<|Name|>",name1).replace("<|Date|>","16-06-2026"))
+
+text="hello world  hi world"
+print(text.find("  "))
+print(text.replace("  "," "))
+
+letter1 = "Dear Harry,\nthis python course is nice.\nThanks!"
+print(letter1)
+
